21a89ace0a0d84aa87c55eaa9571ad33ffabe0be
mentadp <mentavps15@gmail.com>
2023-05-07 16:29:16 +0700

    KETERANGAN

---
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,187 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.txtNamaBarang = New System.Windows.Forms.TextBox
+        Me.cbBarang = New System.Windows.Forms.ComboBox
+        Me.txtHargaBarang = New System.Windows.Forms.TextBox
+        Me.txtJumlahBarang = New System.Windows.Forms.TextBox
+        Me.txtTotal = New System.Windows.Forms.TextBox
+        Me.btnProses = New System.Windows.Forms.Button
+        Me.btnBersih = New System.Windows.Forms.Button
+        Me.btnTutup = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(37, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(75, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Kode Barang :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(37, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Nama Barang :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(37, 89)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(79, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Harga Barang :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(37, 111)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(46, 13)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Jumlah :"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(37, 141)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(37, 13)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "Total :"
+        '
+        'txtNamaBarang
+        '
+        Me.txtNamaBarang.Location = New System.Drawing.Point(151, 59)
+        Me.txtNamaBarang.Name = "txtNamaBarang"
+        Me.txtNamaBarang.Size = New System.Drawing.Size(208, 20)
+        Me.txtNamaBarang.TabIndex = 1
+        '
+        'cbBarang
+        '
+        Me.cbBarang.FormattingEnabled = True
+        Me.cbBarang.Location = New System.Drawing.Point(151, 32)
+        Me.cbBarang.Name = "cbBarang"
+        Me.cbBarang.Size = New System.Drawing.Size(208, 21)
+        Me.cbBarang.TabIndex = 2
+        '
+        'txtHargaBarang
+        '
+        Me.txtHargaBarang.Location = New System.Drawing.Point(151, 82)
+        Me.txtHargaBarang.Name = "txtHargaBarang"
+        Me.txtHargaBarang.Size = New System.Drawing.Size(208, 20)
+        Me.txtHargaBarang.TabIndex = 1
+        '
+        'txtJumlahBarang
+        '
+        Me.txtJumlahBarang.Location = New System.Drawing.Point(151, 108)
+        Me.txtJumlahBarang.Name = "txtJumlahBarang"
+        Me.txtJumlahBarang.Size = New System.Drawing.Size(208, 20)
+        Me.txtJumlahBarang.TabIndex = 1
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Location = New System.Drawing.Point(151, 134)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(208, 20)
+        Me.txtTotal.TabIndex = 1
+        '
+        'btnProses
+        '
+        Me.btnProses.Location = New System.Drawing.Point(40, 205)
+        Me.btnProses.Name = "btnProses"
+        Me.btnProses.Size = New System.Drawing.Size(75, 23)
+        Me.btnProses.TabIndex = 3
+        Me.btnProses.Text = "Proses"
+        Me.btnProses.UseVisualStyleBackColor = True
+        '
+        'btnBersih
+        '
+        Me.btnBersih.Location = New System.Drawing.Point(151, 205)
+        Me.btnBersih.Name = "btnBersih"
+        Me.btnBersih.Size = New System.Drawing.Size(75, 23)
+        Me.btnBersih.TabIndex = 3
+        Me.btnBersih.Text = "Bersih"
+        Me.btnBersih.UseVisualStyleBackColor = True
+        '
+        'btnTutup
+        '
+        Me.btnTutup.Location = New System.Drawing.Point(284, 205)
+        Me.btnTutup.Name = "btnTutup"
+        Me.btnTutup.Size = New System.Drawing.Size(75, 23)
+        Me.btnTutup.TabIndex = 3
+        Me.btnTutup.Text = "Tutup"
+        Me.btnTutup.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(406, 247)
+        Me.Controls.Add(Me.btnTutup)
+        Me.Controls.Add(Me.btnBersih)
+        Me.Controls.Add(Me.btnProses)
+        Me.Controls.Add(Me.cbBarang)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.txtJumlahBarang)
+        Me.Controls.Add(Me.txtHargaBarang)
+        Me.Controls.Add(Me.txtNamaBarang)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtNamaBarang As System.Windows.Forms.TextBox
+    Friend WithEvents cbBarang As System.Windows.Forms.ComboBox
+    Friend WithEvents txtHargaBarang As System.Windows.Forms.TextBox
+    Friend WithEvents txtJumlahBarang As System.Windows.Forms.TextBox
+    Friend WithEvents txtTotal As System.Windows.Forms.TextBox
+    Friend WithEvents btnProses As System.Windows.Forms.Button
+    Friend WithEvents btnBersih As System.Windows.Forms.Button
+    Friend WithEvents btnTutup As System.Windows.Forms.Button
+
+End Class
---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,62 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'Membuat array daftar barang
+        Dim Barang() As String = {"PL01,PUPEL PILOT,1200", "PL02,PULPEN STANDART,1000", "BK01,BUKU AA 60 LBR,3800", "BK02,BUKU SINAR DUNIA 60 LBR,3000"}
+
+        'Memisahkan data dan menambahkannya pada combobox
+        For i As Integer = 0 To Barang.Length - 1
+            Dim data() As String = Barang(i).Split(",")
+            Dim kodeBarang As String = data(0)
+            Dim namaBarang As String = data(1)
+            Dim hargaBarang As Integer = Convert.ToInt32(data(2))
+            cbBarang.Items.Add(kodeBarang & " - " & namaBarang & " - " & hargaBarang.ToString("N0"))
+        Next
+    End Sub
+
+    Private Sub btnProses_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnProses.Click
+        'Memeriksa apakah combobox sudah dipilih
+        If cbBarang.SelectedIndex < 0 Then
+            MessageBox.Show("Silakan pilih barang terlebih dahulu!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        'Mendapatkan data barang yang dipilih dari combobox
+        Dim dataBarang() As String = cbBarang.SelectedItem.ToString.Split("-")
+        Dim kodeBarang As String = dataBarang(0).Trim()
+        Dim namaBarang As String = dataBarang(1).Trim()
+        Dim hargaBarang As Integer = Convert.ToInt32(dataBarang(2).Trim().Replace(".", ""))
+
+        'Mendapatkan data jumlah barang dari textbox
+        Dim jumlahBarang As Integer = Val(txtJumlahBarang.Text)
+
+        'Menghitung total harga dan menampilkan pada textbox
+        Dim totalHarga As Integer = hargaBarang * jumlahBarang
+        txtTotal.Text = totalHarga.ToString("N0")
+    End Sub
+
+    Private Sub btnBersih_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnBersih.Click
+        'Mereset nilai pada combobox dan seluruh textbox
+        cbBarang.SelectedIndex = -1
+        txtNamaBarang.Clear()
+        txtHargaBarang.Clear()
+        txtJumlahBarang.Clear()
+        txtTotal.Clear()
+    End Sub
+
+    Private Sub btnTutup_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnTutup.Click
+        'Menutup program
+        Me.Close()
+    End Sub
+
+    Private Sub cbBarang_SelectedIndexChanged(ByVal sender As Object, ByVal e As EventArgs) Handles cbBarang.SelectedIndexChanged
+        'Mendapatkan data barang yang dipilih dari combobox dan menampilkan pada textbox
+        If cbBarang.SelectedIndex >= 0 Then
+            Dim dataBarang() As String = cbBarang.SelectedItem.ToString.Split("-")
+            txtNamaBarang.Text = dataBarang(1).Trim()
+            txtHargaBarang.Text = dataBarang(2).Trim()
+        Else
+            txtNamaBarang.Clear()
+            txtHargaBarang.Clear()
+        End If
+    End Sub
+End Class
